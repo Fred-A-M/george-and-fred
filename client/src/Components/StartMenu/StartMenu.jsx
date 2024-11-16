16ad573c31d38b667504ff698a0cb92ec5892970
Fred Moore <frederic_archer@hotmail.com>
2024-11-16 20:42:58 +0000

    changes

--- client/src/Components/StartMenu/StartMenu.jsx
+++ client/src/Components/StartMenu/StartMenu.jsx
@@ -11,11 +11,11 @@ export default function StartMenu ({changeState, changeCards}) {
           <img src="./george-and-fred-heads.png" alt="George and Fred's heads" className='head-pics' style={{width: '75%'}}/>
         </div>
         <div className='game-choices'>
-          <h2 className='start-menu-button' onClick={() => {changeState('game'); changeCards(4)} }>
-          Two Cards Each
+          <h2 className='start-menu-button' onClick={() => {changeState('game'); changeCards(18)} }>
+          Nine Cards Each
           </h2>
-          <h2 className='start-menu-button' onClick={() => {changeState('game'); changeCards(8)} }>
-          Four Cards Each 
+          <h2 className='start-menu-button' onClick={() => {changeState('game'); changeCards(12)} }>
+          Six Cards Each 
           </h2>
         </div>
     </div>
